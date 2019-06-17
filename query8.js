e2750bf8d1679a66ffb852f9be445b57b81b0c58
zvladyuha <z.vladyuha@yandex.ru>
2019-06-18 02:05:38 +0300

    add function wrapper

--- query8.js
+++ query8.js
@@ -1,39 +1,43 @@
 // Create a collection with info about flights origins
 // Contains total duration of all flights from specific city, number of flights, average duration of flight
 
-var mapFlightsToOrigin = function() {
-	var key = this.origin;
-	var flightDuration = this.duration;
+function flights_from_origin(){
 
-	var value = { totalDuration: flightDuration, flightsNumber: 1 }
-	emit(key, value);
-	
-};
+	var mapFlightsToOrigin = function() {
+		var key = this.origin;
+		var flightDuration = this.duration;
 
-var reduceOriginFlightsDuration = function(flightOrigin, values) {
-	var durationsSum = Array.sum(values.map(function(value) { return value.totalDuration }));
-	var durationsLength = values.length;
-
-	reducedVal = {};
-
-	reducedVal.totalDuration =  durationsSum;
-	reducedVal.flightsNumber = durationsLength;
-	
-	return reducedVal;
-};
-
-var finalizeFlightsOriginInfo = function(key, reducedVal) {
-	reducedVal.averageDuration = reducedVal.totalDuration / reducedVal.flightsNumber;
-
-	return reducedVal;
-}
-
-
-db.flights.mapReduce(
-	mapFlightsToOrigin,
-	reduceOriginFlightsDuration,
-	{
-		finalize: finalizeFlightsOriginInfo,
-		out: "origin_flights_info",
+		var value = { totalDuration: flightDuration, flightsNumber: 1 }
+		emit(key, value);
+		
 	}
-)
+
+	var reduceOriginFlightsDuration = function(flightOrigin, values) {
+		var durationsSum = Array.sum(values.map(function(value) { return value.totalDuration }));
+		var durationsLength = values.length;
+
+		reducedVal = {};
+
+		reducedVal.totalDuration =  durationsSum;
+		reducedVal.flightsNumber = durationsLength;
+		
+		return reducedVal;
+	}
+
+	var finalizeFlightsOriginInfo = function(key, reducedVal) {
+		reducedVal.averageDuration = reducedVal.totalDuration / reducedVal.flightsNumber;
+
+		return reducedVal;
+	}
+
+	db.flights.mapReduce(
+		mapFlightsToOrigin,
+		reduceOriginFlightsDuration,
+		{
+			finalize: finalizeFlightsOriginInfo,
+			out: "origin_flights_info"
+		}
+	);
+
+	return db.origin_flights_info.find()
+}
